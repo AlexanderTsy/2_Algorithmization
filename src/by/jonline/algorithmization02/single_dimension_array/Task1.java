bfb6b8bca94bc0c04bdb6927e84edef5946afb08
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-22 11:28:31 +0300

    Update algorithmization02.single_dimension_array Task1
    add methods public long sumOfMultipleToK, private int getPositiveInt

--- src/by/jonline/algorithmization02/single_dimension_array/Task1.java
+++ src/by/jonline/algorithmization02/single_dimension_array/Task1.java
@@ -9,38 +9,79 @@ public class Task1 {
 	 */
 
 	/*
-	 * Решение. Запросим значение К с клавиатуры. Числа будут кратными, если остаток
-	 * от деления равен 0.
+	 * Метод вычисляет сумму элементов массива, кратных K
 	 */
-	public static void main(String[] args) {
-		int[] a = { 1, 3, 12, 43, 78, 121, 44, 125, 748, 11, 67 }; // массив A[N]
-		int k = 0; // переменная К
-		int sum = 0; // искомая сумма
-
-		@SuppressWarnings("resource") // не закрываем сканер, использующий System.in
-		Scanner scanner = new Scanner(System.in);
-
-		// Получим число К
-		boolean inputOK = false; // если ввод будет верный, К - натуральное число, то присвоим true
-		do {
-			System.out.println("Введите натуральное число К:");
-			if (scanner.hasNextInt()) {
-				k = scanner.nextInt();
-				if (k > 0) {
-					inputOK = true;
-				} else {
-					System.err.println("Введенное число меньше 0.");
-				}
+	public static long sumOfMultipleToK(int[] arr, int k) {
+		long sum = 0;
+		long previousSum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			// Проверка входных данных
+			if (arr[i] <= 0) {
+				// возможно лучше выбросить исключение, System.out и System.err не синхронизированы
+				System.err.println("В массиве есть элементы не натуральные числа A["+i+"]="+arr[i]);
+				return 0;
 			}
-		} while (!inputOK);
-
-		// вычислим сумму элементов кратных К
-		for (int i = 0; i < a.length; i++) {
-			if (a[i] % k == 0) {
-				sum += a[i];
+			// Числа будут кратными k, если остаток от деления равен 0.
+			if (arr[i] % k == 0) {
+				sum += arr[i];
+			}
+			// проверка переполнения типа long
+			if (sum < previousSum) {
+				System.err.println("Переполнение типа long");
+				return 0;
+			} else {
+				previousSum = sum;
 			}
 		}
+		return sum;
+	}
+	
+	/*
+	 * Метод запрашивает положительное число типа int с клавиатуры
+	 */
+	private static int getPositiveInt(String message) {
+		@SuppressWarnings("resource") // не закрываем сканер, использующий System.in
+		Scanner scanner = new Scanner(System.in);
+		
+		int k = 0;
 
+		// Получим число К > 0
+		do {
+			System.out.println(message);
+			while (!scanner.hasNextInt()) {
+				scanner.next();
+				System.out.println(message);
+			}
+			k = scanner.nextInt();
+		} while (k <= 0);
+		
+		return k;
+	}
+	
+	public static void main(String[] args) {
+		int k; // переменная К
+		long sum; // искомая сумма
+		
+		// Тестовый массив 1
+		int[] a1 = { 1, 3, -65, 43, 78, 121, 44, 125, 748, 11, 67 };
+		// Тестовый массив 2
+		int[] a2 = { 1, 3, Integer.MAX_VALUE-1, Integer.MAX_VALUE-1, Integer.MAX_VALUE-1, Integer.MAX_VALUE-1, Integer.MAX_VALUE-1, 125, 748, 11, 67 };
+		
+		
+		System.out.println("Тестовый массив 1");
+		
+		k = getPositiveInt("Введите положительное целое k >>");
+		
+		// вычислим сумму элементов кратных К
+		sum = sumOfMultipleToK(a1, k);
+		System.out.println("Сумма элементов массива A[N] кратных К=" + k + " равна: " + sum);
+		
+		System.out.println("Тестовый массив 2");
+		
+		k = getPositiveInt("Введите положительное целое k >>");
+		
+		// вычислим сумму элементов кратных К
+		sum = sumOfMultipleToK(a2, k);
 		System.out.println("Сумма элементов массива A[N] кратных К=" + k + " равна: " + sum);
 	}
 
