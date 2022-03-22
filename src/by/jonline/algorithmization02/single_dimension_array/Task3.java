9c770a39dde0e2c97d592287911fa550d26d05a3
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-22 12:38:57 +0300

    Update algorithmization02.single_dimension_array Task3
    Add method calculateNegZeroPos

--- src/by/jonline/algorithmization02/single_dimension_array/Task3.java
+++ src/by/jonline/algorithmization02/single_dimension_array/Task3.java
@@ -1,25 +1,29 @@
 package by.jonline.algorithmization02.single_dimension_array;
 
+/**
+ * 
+ * 3. Algorithmization Одномерные массивы. Задание 3<br/>
+ * Дан массив действительных чисел, размерность которого N.<br/>
+ * Подсчитать, сколько в нем отрицательных, положительных и нулевых элементов.
+ *
+ * @author tsyhanok.aa
+ * 
+ */
 public class Task3 {
-	/*
-	 * 3. Algorithmization Одномерные массивы. Задание 3 Дан массив действительных
-	 * чисел, размерность которого N. Подсчитать, сколько в нем отрицательных,
-	 * положительных и нулевых элементов.
+	
+	/**
+	 * Method calculates number of negative, positive and zero elements in <b>array<b/>
+	 * 
+	 * @param array	array of real numbers
+	 * @return 	arr[0] count of negative<br/>
+	 * 			arr[1] count of zeros<br/>
+	 * 			arr[2] count of positive
 	 */
-
-	private static int n = 10000; // размерность массива N
-	private static double[] array = new double[n];
-
-	public static void main(String[] args) {
-		// Для демонстрации работы алгоритма
-		// зададим содержимое массива случайным образом
-		for (int i = 0; i < array.length; i++) {
-			array[i] = (Math.random() - 0.5) * 10000;
-		}
-
+	public static int[] calculateNegZeroPos(final double[] array){
+		int[] result = {0,0,0};
 		// Объявим и инициализируем переменные количества элементов
-		int countZero = 0; // нули
 		int countNegative = 0; // отрицательные
+		int countZero = 0; // нули
 		int countPositive = 0; // положительные
 
 		// подсчитаем количество элементов
@@ -32,9 +36,34 @@ public class Task3 {
 				countPositive++;
 			}
 		}
-		System.out.println("Количество отрицательных элементов: " + countNegative);
-		System.out.println("Количество нулей: " + countZero);
-		System.out.println("Количество положительных элементов: " + countPositive);
+		
+		result[0] = countNegative;
+		result[1] = countZero;
+		result[2] = countPositive;
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+		int[] result;
+		
+		/* Для демонстрации работы алгоритма
+		 * зададим содержимое массива случайным образом
+		 */
+		int n = 10000; // размерность массива N
+		double[] array = new double[n];
+		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (Math.random() - 0.5) * 10000;
+		}
+
+		// подсчитаем количество элементов
+		result = calculateNegZeroPos(array);
+		
+		System.out.println("Количество отрицательных элементов: " + result[0]);
+		System.out.println("Количество нулей: " + result[1]);
+		System.out.println("Количество положительных элементов: " + result[2]);
 	}
 
 }
