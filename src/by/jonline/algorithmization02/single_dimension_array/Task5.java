12855c1b5e197f2e34a93caf5a1e9283fd382c5a
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-22 13:16:30 +0300

    Upd algorithmization02.single_dimension_array Task5
    Rearrange code

--- src/by/jonline/algorithmization02/single_dimension_array/Task5.java
+++ src/by/jonline/algorithmization02/single_dimension_array/Task5.java
@@ -2,42 +2,14 @@ package by.jonline.algorithmization02.single_dimension_array;
 
 import java.util.Scanner;
 
+/**
+ * 5. Algorithmization Одномерные массивы. Задание 5 Даны целые числа a1, a2,
+ * ..., an. Вывести на печать только те числа, для которых ai>i.
+ * 
+ * @author tsyhanok.aa
+ * 
+ */
 public class Task5 {
-	/*
-	 * 5. Algorithmization Одномерные массивы. Задание 5 Даны целые числа a1, a2,
-	 * ..., an. Вывести на печать только те числа, для которых ai>i.
-	 */
-
-	public static void main(String[] args) {
-		int arrayLength;
-		int minVal;
-		int maxVal;
-		int[] arr;
-		// запросим  тестовые данные
-		arrayLength = enterIntFromConsole("Введите размер массива");
-		minVal = enterIntFromConsole("Введите минимум для генерации числа");
-		maxVal = enterIntFromConsole("Введите максимум для генерации числа");
-		// сгенерируем тестовый массив
-		arr = generateArray(arrayLength, minVal, maxVal);
-		
-		// выведем числа для которых ai>i
-		printGreaterThanI(arr);
-
-	}
-	
-	// метод запрашивает ввести целое число с сообщением message
-	private static int enterIntFromConsole(String message) {
-		
-		@SuppressWarnings("resource") // не закрываем сканер, использующий System.in
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print(message);
-		while (!scanner.hasNextInt()) {
-			System.out.print(message);
-			scanner.next();
-		}
-		return scanner.nextInt(); 
-	}
 
 	// метод выводит в стандартный вывод числа, значение которых больше их номера
 	public static void printGreaterThanI(int[] array) {
@@ -48,19 +20,54 @@ public class Task5 {
 		}
 	}
 
+	// метод запрашивает ввести целое число с сообщением message
+	private static int enterIntFromConsole(String message) {
+
+		@SuppressWarnings("resource") // не закрываем сканер, использующий System.in
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print(message);
+		while (!scanner.hasNextInt()) {
+			System.out.print(message);
+			scanner.next();
+		}
+		return scanner.nextInt();
+	}
+
 	// метод для генерации массива целых чисел
 	// размер массива length, числа в пределах min, max
 	private static int[] generateArray(int length, int min, int max) {
 		int[] arr = new int[length];
+
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = getRandomNumber(min, max);
 		}
+
 		return arr;
 	}
 
 	// метод для генерации целых чисел в границах min, max
 	private static int getRandomNumber(int min, int max) {
 		return (int) ((Math.random() * (max - min)) + min);
+	}
+
+	public static void main(String[] args) {
+		int arrayLength;
+		int minVal;
+		int maxVal;
+		int[] arr;
+
+		// запросим тестовые данные
+		arrayLength = enterIntFromConsole("Введите размер массива");
+		minVal = enterIntFromConsole("Введите минимум для генерации числа");
+		maxVal = enterIntFromConsole("Введите максимум для генерации числа");
+
+		// сгенерируем тестовый массив
+		arr = generateArray(arrayLength, minVal, maxVal);
+
+		// выведем числа для которых ai>i
+		printGreaterThanI(arr);
+
 	}
 
 }
