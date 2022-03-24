0ddba63fe9c0ca2c1e3e8f7a1429168a8d436872
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-24 17:01:09 +0300

    Add algorithmization02.multidimensional_array Task16
    Task only, no code

---
+++ src/by/jonline/algorithmization02/multidimensional_array/Task16.java
@@ -0,0 +1,31 @@
+package by.jonline.algorithmization02.multidimensional_array;
+
+/**
+ * 2 Algorithmization Массивы массивов. Задание 16<br/>
+ * Магическим квадратом порядка n называется квадратная матрица размера n x n,
+ * составленная из чисел 1, 2, 3, ..., n<sup>2</sup> так, что суммы по каждому
+ * столбцу, каждой строке и каждой из двух больших диагоналей равны между собой.
+ * Построить такой квадрат.
+ * Пример магического квадрата порядка 3:
+ * 
+ * <table>
+ * <tr><td>6</td><td>1</td><td>8</td></tr>
+ * <tr><td>7</td><td>5</td><td>3</td></tr>
+ * <tr><td>2</td><td>9</td><td>4</td></tr>
+ * </table>
+ * 
+ * @author tsyhanok.aa
+ *
+ */
+public class Task16 {
+	
+	public static int[][] makeMagicSquare() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
