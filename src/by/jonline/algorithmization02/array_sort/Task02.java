865a2b06dc7f625d04b72afead1fe073890c110f
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-25 17:27:44 +0300

    Add algorithmization02.array_sort Task02
    Not working properly. Need fix

---
+++ src/by/jonline/algorithmization02/array_sort/Task02.java
@@ -0,0 +1,97 @@
+package by.jonline.algorithmization02.array_sort;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * 2 Algorithmization Одномерные массивы. Сортировки. Задание 2<br/>
+ * Даны две последовательности a<sub>1</sub> &leq; a<sub>2</sub> &leq; ... &leq; a<sub>n</sub>
+ * и b<sub>1</sub> &leq; b<sub>2</sub> &leq; ... &leq; b<sub>m</sub>.
+ * Образовать из них новую последовательность чисел так, чтобы она тоже была неубывающей.
+ * <i>Примечение.</i> Дополнительный массив не использовать.
+ * 
+ * @author tsyhanok.aa
+ *
+ */
+public class Task02 {
+	
+	public static int[] joinArrays(int[] a, int[] b) {
+		int[] result;
+		int aCount;
+		int bCount;
+		
+		if( Objects.isNull(a) && Objects.isNull(b)) {
+			return null;
+		}
+		
+		if( Objects.isNull(a) && !Objects.isNull(b)) {
+			return b.clone();
+		}
+		
+		if( !Objects.isNull(a) && Objects.isNull(b)) {
+			return a.clone();
+		}
+		
+		try {
+			Math.addExact(a.length,b.length);
+		} catch (ArithmeticException e) {
+			System.err.println("Sum of arrays lengths exceeds maximal index number.");
+			return null;
+		}
+		
+		result = new int[a.length+b.length];
+		aCount = 0;
+		bCount = 0;
+		// TODO malfunction at the end of arrays - fix.
+		for (int i = 0; i < result.length; i++) {
+			if(a[aCount] < b[bCount] && aCount < a.length) {
+				result[i] = a[aCount];
+				aCount++;
+			} else {
+				result[i] = b[bCount];
+				bCount++;
+			}
+		}
+		
+		return result;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] array1;
+		int[] array2;
+		int[] result;
+		
+		System.out.println("Test 1. First array longer");
+		array1 = new int[] {1, 20, 21, 23, 30, 40, 50};
+		array2 = new int[] {11, 22, 33, 44};
+		result = joinArrays(array1, array2);
+		System.out.println(Arrays.toString(result));
+		
+		System.out.println("Test 2. First array shorter");
+		array1 = new int[] {11, 22, 33, 44};
+		array2 = new int[] {1, 20, 21, 23, 30, 40, 50};
+		result = joinArrays(array1, array2);
+		System.out.println(Arrays.toString(result));
+		
+		System.out.println("Test 3. Second array is null");
+		array1 = new int[] {11, 22, 33, 44};
+		array2 = null;
+		result = joinArrays(array1, array2);
+		System.out.println(Arrays.toString(result));
+		
+		System.out.println("Test 4. First array is null");
+		array1 = null;
+		array2 = new int[] {11, 22, 33, 44};
+		result = joinArrays(array1, array2);
+		System.out.println(Arrays.toString(result));
+		
+		System.out.println("Test 5. Two arrays are null");
+		array1 = null;
+		array2 = null;
+		result = joinArrays(array1, array2);
+		System.out.println(Arrays.toString(result));
+
+	}
+
+}
