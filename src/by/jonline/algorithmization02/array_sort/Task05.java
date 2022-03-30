556124be380a9c8f01da81094e866045834ed64e
Alexandr Tsyhanok <shurius@gmail.com>
2022-03-30 12:06:40 +0300

    Update algorithmization02.array_sort Task05
    Methods work. Test passed.

--- src/by/jonline/algorithmization02/array_sort/Task05.java
+++ src/by/jonline/algorithmization02/array_sort/Task05.java
@@ -21,15 +21,53 @@ import java.util.Objects;
  */
 public class Task05 {
 
-	public static void insertionSort(int[] array) {
+	/**
+	 * Method performs insertion sort. Each value is inserted into result array
+	 * depending on its value. Insertion position is found using binary search.
+	 * @param array
+	 * @return sorted array
+	 */
+	public static int[] insertionSort(int[] array) {
+		if(Objects.isNull(array)) {
+			return null;
+		}
 		
+		int[] result = new int[0];
+		int insertAt;
+		
+		for(int i = 0; i < array.length; i++) {
+			insertAt = binarySearch(array[i],result);
+			result = insertValueAt(insertAt, array[i],result);
+		}
+		
+		return result;
 	}
 	
+	/**
+	 * Method inserts a value at given index into array 
+	 * @param index of insertion
+	 * @param value to be inserted
+	 * @param array for insertion
+	 * @return array with inserted value
+	 */
+	private static int[] insertValueAt(int index, int value, int[] array) {
+		int[] result = new int[array.length+1];
+		for (int i = 0; i < result.length; i++) {
+			if(i < index) {
+				result[i] = array[i];
+			} else if (index == i) {
+				result[i] = value;
+			} else {
+				result[i] = array[i-1];
+			}
+		}
+		return result;
+	}
+
 	public static int binarySearch(int search, int[] array) {
-		int result;
 		int midPoint;
-		int[] left_arr;
-		int[] right_arr;
+		int start;
+		int finish;
 		
 		if(Objects.isNull(array) || Objects.isNull(search)) {
 			System.err.println("Null objects");
@@ -39,42 +77,52 @@ public class Task05 {
 			return 0;
 		}
 		
-		midPoint = (array.length-1)/2;
-		while (midPoint > 0 && midPoint < array.length) {
-			if(search < array[midPoint]) {
-				midPoint = midPoint/2;
-			} else {
-				midPoint += midPoint/2;
+		start = 0;
+		finish = array.length - 1;
+		midPoint = (start + finish)/2;
+		
+		while (start <= finish) {
+			if(search <= array[start]) {
+				return start;
 			}
+			if(search >= array[finish]) {
+				return finish+1;
+			}
+			if(search < array[midPoint]) {
+				finish = midPoint - 1;
+			} else if (search == array[midPoint]) {
+				return midPoint;
+			} else {
+				start = midPoint + 1;
+			}
+			midPoint = (start + finish)/2;
 		}
-		
-		
 		return midPoint;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr;
 
+		System.out.println("Test binarySearch");
+		arr = new int[] { 0, 1, 3, 5 };
+		System.out.println(binarySearch(1, arr));
+
 		arr = new int[] { 15, 12, 1, 6, 60, 0, -15, 67, 100 };
 		System.out.println("Test 1 random values");
-		insertionSort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(insertionSort(arr)));
 
 		System.out.println("Test 2 Values ordered descending");
 		arr = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-		insertionSort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(insertionSort(arr)));
 
 		System.out.println("Test 3 Empty array");
 		arr = new int[] {};
-		insertionSort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(insertionSort(arr)));
 
 		System.out.println("Test 4 Array is null");
 		arr = null;
-		insertionSort(arr);
-		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(insertionSort(arr)));
 	}
 
 }
