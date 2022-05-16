e28125c580c9990e7e937dbb4d6653faf9d313cf
Alexandr Tsyhanok <shurius@gmail.com>
2022-05-16 07:56:04 +0300

    Add algorithmization02.method_decomposition Task13
    Task text only

---
+++ src/by/jonline/algorithmization02/method_decomposition/Task13.java
@@ -0,0 +1,24 @@
+package by.jonline.algorithmization02.method_decomposition;
+
+/**
+ * 2 Algorithmization Одномерные массивы. Декомпозиция с использованием методов
+ * (подпрограммы). Задание 13<br/>
+ * Два простых числа называются "близнецами", если они отличаются друг от друга
+ * на 2 (например, 41 и 43). Найти и напечатать все пары "близнецов" из отрезка
+ * [n, 2n], где n - заданное натуральное число больше 2. Для решения задачи
+ * использовать декомпозицию.
+ * 
+ * @author Aliaxandr Tsyhanok shurius@gmail.com
+ *
+ */
+public class Task13 {
+	
+	public static void printTwins(int n) {
+		
+	}
+	
+	private static int[] getPrimesNto2N(int n) {
+		return null;
+	}
+
+}
